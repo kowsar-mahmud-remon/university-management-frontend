08322e38cfd98b61a5eebf03c8d2403eef9f202a
creativerze <creativerze@gmail.com>
2023-09-26 12:18:47 +0600

    added layout

---
+++ src/app/(withlayout)/super-admin/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SuperAdminPage = () => {
+  return (
+    <div>
+      <h1>This page is for Super Admin</h1>
+    </div>
+  );
+};
+
+export default SuperAdminPage;
---
+++ src/components/ui/Contents.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "antd";
+const { Content } = Layout;
+
+const Contents = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Content style={{ minHeight: "100vh", color: "black" }}>{children}</Content>
+  );
+};
+
+export default Contents;
