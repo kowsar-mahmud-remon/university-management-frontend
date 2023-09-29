a4104a17789050fbe94779aee76258daca09784c
creativerze <creativerze@gmail.com>
2023-09-29 12:53:34 +0600

    added form and roboto font

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,67 @@
-import React from "react";
+"use client";
+
+import { Button, Col, Row } from "antd";
+import loginImage from "../../assets/login-image.png";
+import Image from "next/image";
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import { SubmitHandler } from "react-hook-form";
+
+type FormValues = {
+  id: string;
+  password: string;
+};
 
 const LoginPage = () => {
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    try {
+      console.log(data);
+    } catch (error) {}
+  };
   return (
-    <div>
-      <h1>This is login page</h1>
-    </div>
+    <Row
+      justify="center"
+      align="middle"
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <Col sm={12} md={16} lg={10}>
+        <Image src={loginImage} width={500} alt="login image" />
+      </Col>
+      <Col sm={12} md={8} lg={8}>
+        <h1
+          style={{
+            margin: "15px 0px",
+          }}
+        >
+          First Login your account
+        </h1>
+        <div className="">
+          <Form submitHandler={onSubmit}>
+            <div className="">
+              <FormInput name="id" type="text" size="large" label="user Id" />
+            </div>
+            <div
+              className=""
+              style={{
+                margin: "15px 0px",
+              }}
+            >
+              <FormInput
+                name="password"
+                type="password"
+                size="large"
+                label="user Password"
+              />
+            </div>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
