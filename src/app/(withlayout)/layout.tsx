9e2dbf30018a73f33d0716c43a03dbc0c23065f7
creativerze <creativerze@gmail.com>
2023-09-30 20:54:55 +0600

    added private route

--- src/app/(withlayout)/layout.tsx
+++ src/app/(withlayout)/layout.tsx
@@ -2,9 +2,29 @@
 
 import Contents from "@/components/ui/Contents";
 import Sidebar from "@/components/ui/Sidebar";
+import { isLoggedIn } from "@/services/auth.service";
 import { Layout } from "antd";
+import { useRouter } from "next/navigation";
+
+import { useEffect, useState } from "react";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const userLoggedIn = isLoggedIn();
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!userLoggedIn) {
+      router.push("/login");
+    }
+    setIsLoading(true);
+  }, [router, isLoading]);
+
+  if (!isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <Layout hasSider>
       <Sidebar></Sidebar>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,6 +8,7 @@ import FormInput from "@/components/Forms/FormInput";
 import { SubmitHandler } from "react-hook-form";
 import { useUserLoginMutation } from "@/redux/api/authApi";
 import { isLoggedIn, storeUserInfo } from "@/services/auth.service";
+import { useRouter } from "next/navigation";
 
 type FormValues = {
   id: string;
@@ -15,12 +16,17 @@ type FormValues = {
 };
 
 const LoginPage = () => {
-  console.log(isLoggedIn());
+  // console.log(isLoggedIn());
   const [userLogin] = useUserLoginMutation();
+  const router = useRouter();
 
   const onSubmit: SubmitHandler<FormValues> = async (data: any) => {
     try {
       const res = await userLogin({ ...data }).unwrap();
+
+      if (res?.data?.accessToken) {
+        router.push("/profile");
+      }
 
       storeUserInfo({ accessToken: res?.data?.accessToken });
       // console.log(res);
